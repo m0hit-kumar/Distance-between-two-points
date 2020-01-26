9face6b33a60c9841ea6ac77830502e367c5bcfb
m0hit-kumar <60257288+m0hit-kumar@users.noreply.github.com>
2020-01-26 06:18:05 -0800

    program to calculate distance between two points
    c++ program to find distance between two point

---
+++ .github/workflows/distance between two coordinates.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<math.h>
+int main()
+{double distance,x1,x2,y1,y2;
+count<<"enter coordinates of point 1:";
+cin>>x1>>x2;
+count<<"enter coordinates of point 2:";
+cin>>y1>>y2;
+distance=sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2);                              //formula
+cout<<"distance between the point one ("<<x1<<","<<y1<<") and point 2 ("<<x2<<","<<y2<<") is :" <<distance<<endl;
+returm 0 ;
+}
+
+
